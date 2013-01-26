f4e5e838598db2f75625221a3620294d2e9836e0
UH <ullas.holla@gmail.com>
2013-01-26 23:01:05 +1000

    min.py: Got it working. Performance is abysmal

--- ullas/fb-cup-2013/min.py
+++ ullas/fb-cup-2013/min.py
@@ -6,25 +6,33 @@ def doIt(infile):
     f = open(infile)
     n = int(f.readline())
     for i in range(1,n+1):
+        #print('_____________________')
         n, k = f.readline().split(' ')
         n, k = int(n), int(k)
         a, b, c, r = f.readline().split(' ')
         a, b, c, r = int(a), int(b), int(c), int(r)
-        print(n, k, a, b, c, r)
+        #print(n, k, a, b, c, r)
 
-        m = []
-        min = a
-        m.append(a)
-        for i in range(1, k):
-            e = (b*m[i-1]+c)%r
-            if min > e: min = e
+        m = [a]
+        prev = a
+        for j in range(1, k):
+            e = (b*prev+c)%r
+#            m.add(e)
             m.append(e)
-        print(m)
-        m = set(m)
-        all = set(range(n))
-        t = list(all.difference(m))
-        ans = t[n-k-1]
-        print('Case #{0}: {1}'.format(i, ans))
+            prev = e
+
+        for j in range(k, n):
+            copy = m[:]
+            copy.sort()
+            for l in range(n):
+                if l not in copy:
+                    break
+            m.append(l)
+            m.pop(0)
+        
+        ans = m.pop()
+        print("Case #{0}: {1}".format(i, ans))
+
 
 if __name__ == '__main__':
     doIt(sys.argv[1])
