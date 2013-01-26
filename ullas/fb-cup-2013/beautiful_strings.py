4c99afa5266609fb40fed1779f31dd1f6e7ae2db
UH <ullas.holla@gmail.com>
2013-01-26 11:02:08 +1000

    Beautiful Strings: Ver 1 of solution

---
+++ ullas/fb-cup-2013/beautiful_strings.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys
+
+def doIt(infile):
+    f = open(infile)
+    n = int(f.readline())
+    for i in range(1,n+1):
+        l = f.readline()
+        d = {}
+        for c in l:
+            c = c.lower()
+            if c.isalpha():
+                d[c] = d.get(c,0) + 1
+#        print(d)
+        counts = [x for x in d.values()]
+        counts.sort()
+        counts.reverse()
+#        print(counts)
+        sum, val = 0, 26
+        for x in counts:
+            sum += x*val
+            val -= 1
+        print(sum)
+
+if __name__ == '__main__':
+    doIt(sys.argv[1])
+    
